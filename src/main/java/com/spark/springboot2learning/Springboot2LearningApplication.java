9eb111bef6d66ffcb83ce027843a11e70e140e06
initiald0824 <initiald0824@163.com>
2019-07-03 22:52:17 +0800

    *

---
+++ src/main/java/com/spark/springboot2learning/Springboot2LearningApplication.java
@@ -0,0 +1,13 @@
+package com.spark.springboot2learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot2LearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot2LearningApplication.class, args);
+    }
+
+}
