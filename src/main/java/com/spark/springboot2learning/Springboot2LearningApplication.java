de9615a5661b5636d6d9b8f93c1e45633a2abaa8
initiald0824 <initiald0824@163.com>
2019-07-06 18:05:41 +0800

    *

--- src/main/java/com/spark/springboot2learning/Springboot2LearningApplication.java
+++ src/main/java/com/spark/springboot2learning/Springboot2LearningApplication.java
@@ -3,11 +3,12 @@ package com.spark.springboot2learning;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author initiald0824
+ */
 @SpringBootApplication
 public class Springboot2LearningApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(Springboot2LearningApplication.class, args);
     }
-
 }
