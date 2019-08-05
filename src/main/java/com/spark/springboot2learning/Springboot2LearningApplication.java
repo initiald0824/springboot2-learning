cab6947a42240f9cb771af435003dd06ce8c5424
initiald0824 <initiald0824@163.com>
2019-08-05 20:06:17 +0800

    *

--- src/main/java/com/spark/springboot2learning/Springboot2LearningApplication.java
+++ src/main/java/com/spark/springboot2learning/Springboot2LearningApplication.java
@@ -6,12 +6,16 @@ import com.spark.springboot2learning.chap4.aspect.MyAspect2;
 import com.spark.springboot2learning.chap4.aspect.MyAspect3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * @author initiald0824
  */
 @SpringBootApplication(scanBasePackages= {"com.spark.springboot2learning.chap5"})
+@EnableJpaRepositories(basePackages = "com.spark.springboot2learning.chap5.dao")
+@EntityScan(basePackages = "com.spark.springboot2learning.chap5.pojo")
 public class Springboot2LearningApplication {
 
     /**
