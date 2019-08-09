71b1ea0650af0be6e965774405ed1614cb563645
initiald0824 <initiald0824@163.com>
2019-08-09 09:23:05 +0800

    Merge branch 'master' of H:\prj\springboot2-learning with conflicts.

--- src/main/java/com/spark/springboot2learning/chap7/controller/RedisController.java
+++ src/main/java/com/spark/springboot2learning/chap7/controller/RedisController.java
@@ -1,118 +1,65 @@
 package com.spark.springboot2learning.chap7.controller;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.connection.RedisZSetCommands;
-import org.springframework.data.redis.core.*;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.core.RedisOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.core.SessionCallback;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import redis.clients.jedis.Jedis;
 
-import java.util.*;
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author initiald0824
- * @date 2019/8/7 12:09
+ * @date 2019/8/8 11:16
  */
 @RestController
 @RequestMapping("/redis")
-public class RedisController {
+public class RedisController implements InitializingBean {
 
     @Autowired
-    private RedisTemplate redisTemplate = null;
+    private RedisTemplate redisTemplate;
 
     @Autowired
-    private StringRedisTemplate stringRedisTemplate = null;
+    private StringRedisTemplate stringRedisTemplate;
 
-    @RequestMapping("/test")
-    public String test () {
-        return "test";
-    }
 
-    @RequestMapping("/stringAndHash")
-    public Map<String, Object> testStringAndHash() {
-        redisTemplate.opsForValue().set("key1", "value1");
-        redisTemplate.opsForValue().set("int_key", "1");
-        stringRedisTemplate.opsForValue().set("int", "1");
-        stringRedisTemplate.opsForValue().increment("int", 1);
-
-        // 获取底层Jedis连接
-        Jedis jedis = (Jedis)stringRedisTemplate.getConnectionFactory().getConnection().getNativeConnection();
-        jedis.decr("int");
-        Map<String, String> hash = new HashMap<String, String>();
-        hash.put("field1", "value1");
-        hash.put("field2", "value2");
-
-        stringRedisTemplate.opsForHash().putAll("hash", hash);
-        stringRedisTemplate.opsForHash().put("hash", "field3", "value3");
-
-        BoundHashOperations hashOperations = stringRedisTemplate.boundHashOps("hash");
-
-        hashOperations.delete("field1", "field2");
-        hashOperations.put("field4", "value5");
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("success", true);
-        return map;
-    }
-
-    @RequestMapping("/list")
-    public Map<String, Object> testList() {
-        stringRedisTemplate.opsForList().leftPushAll(
-                "list1", "v2", "v4", "v6", "v8", "v10"
-        );
-        stringRedisTemplate.opsForList().rightPushAll(
-                "list2", "v1", "v2", "v3", "v4", "v5", "v6"
-        );
-
-        BoundListOperations listOperations = stringRedisTemplate.boundListOps("list2");
-        Object result1 = listOperations.rightPop();
-        Object result2 = listOperations.index(1);
-        listOperations.leftPush("v0");
-        Long size = listOperations.size();
-        List elements = listOperations.range(0, size-2);
-        Map<String, Object> map = new HashMap<String, Object>();
-        map.put("success", true);
-        return map;
-    }
-
-    @RequestMapping("/zset")
-    public Map<String, Object> testZset() {
-        Set<ZSetOperations.TypedTuple<String>> typedTupleSet = new HashSet<>();
-        for (int i = 0; i <= 9; i++) {
-            // 分数
-            double score = i * 0.1;
-            // 创建一个TypedTuple对象，存入值和分数
-            ZSetOperations.TypedTuple<String> typedTuple = new DefaultTypedTuple<>("value" + i, score);
-            typedTupleSet.add(typedTuple);
-        }
-
-        // 往有序集合插入元素
-        stringRedisTemplate.opsForZSet().add("zset1", typedTupleSet);
-        // 绑定zset1有序集合操作
-        BoundZSetOperations<String, String> zSetOperations = stringRedisTemplate.boundZSetOps("zset1");
-
-        // 增加一个元素
-        zSetOperations.add("value10", 0.26);
-        Set<String> setRange = zSetOperations.range(1, 6);
-        // 按分数排序获取有序集合
-        Set<String> setScore = zSetOperations.rangeByScore(0.2, 0.6);
-        // 定义值范围
-        RedisZSetCommands.Range range = new RedisZSetCommands.Range();
-        range.gt("value3");
-
-        range.lte("value8");
-        // 按值排序，请注意这个排序是按字符串排序
-        Set<String> setLex = zSetOperations.rangeByLex(range);
-        // 删除元素
-        zSetOperations.remove("value9", "value2");
-        Double score = zSetOperations.score("value8");
-        // 在下标区间下，按分数排序，同时返回value和score
-        Set<ZSetOperations.TypedTuple<String>> rangeSet = zSetOperations.rangeWithScores(1, 6);
-        // 在分数区间下，按分数排序，同时返回value和score
-        Set<ZSetOperations.TypedTuple<String>> scoreSet = zSetOperations.rangeByScoreWithScores(1, 6);
-        // 按从大到小排序
-        Set<String> reverseSet = zSetOperations.reverseRange(2, 8);
+    @RequestMapping("/multi")
+    public Map<String, Object> testMulti() {
+        redisTemplate.opsForValue().set("key1", "1");
+        List list = (List) redisTemplate.execute(new SessionCallback() {
+            @Override
+            public Object execute(RedisOperations redisOperations) throws DataAccessException {
+                // 设置监控key1
+                redisOperations.watch("key1");
+                redisOperations.opsForValue().set("key1", "value1");
+                // 开启事务，在exec命令执行前，全部都只是进入队列
+                redisOperations.multi();
+                redisOperations.opsForValue().set("key2", "value2");
+                Object value2 = redisOperations.opsForValue().get("key2");
+                System.out.println("命令在队列，所以value为null 【" + value2 + "】");
+                redisOperations.opsForValue().set("key3", "value3");
+                Object value3 = redisOperations.opsForValue().get("key3");
+                System.out.println("命令在队列，所以value为null【" + value3 + "】");
+                return redisOperations.exec();
+            }
+        });
+        System.out.println(list);
         Map<String, Object> map = new HashMap<>();
         map.put("success", true);
         return map;
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        redisTemplate.setKeySerializer(redisTemplate.getStringSerializer());
+        redisTemplate.setValueSerializer(redisTemplate.getStringSerializer());
     }
 }
